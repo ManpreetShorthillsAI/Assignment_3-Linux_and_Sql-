abd45f47826435ccf5da730c286eac9a1a1127e8
DikshaShorthillsAI <diksha.shashwat@shorhills.ai>
2024-09-30 20:28:49 +0530

    SQL Task

---
+++ SQL_Diksha/SQL-2.1.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    city VARCHAR(50)
+);
+INSERT INTO customers (customer_id, customer_name, city) VALUES
+(101, 'John Doe', 'New York'),
+(102, 'Jane Smith', 'Los Angeles'),
+(103, 'Sam Brown', 'Chicago'),
+(104, 'Nancy White', 'Miami'),
+(105, 'Paul Black', 'Boston');
